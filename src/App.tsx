ba7158b2158afb1fb72d46c83730b27f1f99c2c5
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-04-07 00:18:45 -0400

    added the todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+      name: string
+      age: number
+      img: string
+      note?: string
+  }[]
+}
+
 
 function App() {
-  return (
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Oprah Winfrey",
+      age: 68,
+      img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSFfGocW9ksVq05n5MNceO4QJPkrlEDwmtsUfRTqVbEiC7KsRX2",
+      note: "Oprah Winfrey is the first black woman billionaire and the richest African-American woman.",
+    },
+    {
+      name: "Lionel Messi",
+      age: 34,
+      img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR_BSXPlBjoBeJruSaCamv7kQuMNjoIIWX0CITXUVoapFCbRM9g"
+    }
+  ])
+
+  return ( 
     <div className="App">
-      welcome to the party
+      <h1>People Invited to my Party</h1>
+      <List people={people}/>
+      <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
